6a006904a5e7e0d7f743aab4002ef2cd4651b412
Randy Taylor <randyisbusy@gmail.com>
2019-08-03 19:39:16 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -3,11 +3,11 @@ class Book
   attr_accessor :author, :page_count, :genre
   attr_reader :title
 
-  def initialize(title, GENRES)
+  def initialize(title)
     @title = title
-    @GENRES = GENRES
+  
   end
-
+  GENRES = []
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
