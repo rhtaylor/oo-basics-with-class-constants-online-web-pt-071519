ce932952c8489344341f0cd3b0a582f42a65b62b
Randy Taylor <randyisbusy@gmail.com>
2019-08-03 20:31:31 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,11 +1,10 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
-
-
   BRANDS = []
   BRANDS.uniq!
   def initialize(brand)
     @brand = brand
+    BRANDS << @brand
   end
   def brand
     @brand
