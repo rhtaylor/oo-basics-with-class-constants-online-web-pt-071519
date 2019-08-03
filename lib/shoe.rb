66996dd9a255a2dd9cf6d1add904805eb55e75b1
Randy Taylor <randyisbusy@gmail.com>
2019-08-03 21:07:19 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -3,7 +3,7 @@ class Shoe
   BRANDS = []
   def initialize(brand)
     @brand = brand
-    BRANDS << BRANDS.include?(brand)? nil : brand
+    BRANDS << brand if !BRANDS.include? brand
   end
   def brand
     @brand
